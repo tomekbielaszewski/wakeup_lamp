80e388ca7e1ba5199bcbbdc0c6d9b2277344d9e0
Grizwold <tomekbielaszewski@gmail.com>
2018-12-19 10:50:49 +0100

    RaspberryPi PWM support

---
+++ src/main/java/pl/grizwold/wakeup_lamp/logic/RaspberryPi.java
@@ -0,0 +1,35 @@
+package pl.grizwold.wakeup_lamp.logic;
+
+import com.pi4j.io.gpio.*;
+import com.pi4j.util.CommandArgumentParser;
+import com.pi4j.wiringpi.Gpio;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class RaspberryPi {
+    public static final int MAX_PWM_RATE = 1024;
+
+//    private final GpioPinPwmOutput pwm;
+
+    public RaspberryPi() {
+//        final GpioController gpio = GpioFactory.getInstance();
+//        Pin pin = CommandArgumentParser.getPin(
+//                RaspiPin.class,
+//                RaspiPin.GPIO_01);
+//        this.pwm = gpio.provisionPwmOutputPin(pin);
+//        Gpio.pwmSetMode(Gpio.PWM_MODE_BAL);
+//        this.pwm.setPwm(0);
+//
+//        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+//            pwm.setPwm(0);
+//            gpio.shutdown();
+//        }));
+    }
+
+    public void setPWM(int rate) {
+//        this.pwm.setPwm(rate);
+        log.info("PWM rate is: {}", rate);
+    }
+}

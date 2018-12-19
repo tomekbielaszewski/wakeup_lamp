0a0c7302545c7f59a851ebf1968a41d6f1d44809
Grizwold <tomekbielaszewski@gmail.com>
2018-12-19 11:06:05 +0100

    logging REST endpoints

--- src/main/java/pl/grizwold/wakeup_lamp/rest/TimeResource.java
+++ src/main/java/pl/grizwold/wakeup_lamp/rest/TimeResource.java
@@ -1,24 +1,27 @@
 package pl.grizwold.wakeup_lamp.rest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 
+@Slf4j
 @RestController
 @RequestMapping("/time")
 public class TimeResource {
 
     @GetMapping
     public LocalDateTime getTime() {
+        log.info("Getting local time");
         return LocalDateTime.now();
     }
 
     @PutMapping
     public ResponseEntity<String> updateTime(@RequestBody String time) {
         try {
+            log.info("Updating system time to {}", time);
             Runtime.getRuntime().exec(String.format("date -s '%s'", time));
         } catch (IOException e) {
             return ResponseEntity.status(400).body(e.getMessage());
--- src/main/java/pl/grizwold/wakeup_lamp/rest/WakeUpResource.java
+++ src/main/java/pl/grizwold/wakeup_lamp/rest/WakeUpResource.java
@@ -1,11 +1,13 @@
 package pl.grizwold.wakeup_lamp.rest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.grizwold.wakeup_lamp.logic.RaspberryPi;
 import pl.grizwold.wakeup_lamp.logic.WakeUpService;
 import pl.grizwold.wakeup_lamp.model.WakeUpWeek;
 
+@Slf4j
 @RestController
 @RequestMapping("/wakeup")
 public class WakeUpResource {
@@ -18,21 +20,25 @@ public class WakeUpResource {
 
     @GetMapping
     public WakeUpWeek getWakeUpConfig() {
+        log.info("Getting current wake up config");
         return wakeUpService.getWakeUpWeek();
     }
 
     @GetMapping("/{pwm}")
     public void overwritePWM(@PathVariable("pwm") int pwm) {
+        log.info("Setting PWM to {}", pwm);
         raspberryPi.setPWM(pwm);
     }
 
     @PutMapping
     public WakeUpWeek updateWakeUpConfig(@RequestBody WakeUpWeek wakeUpWeek) {
+        log.info("Updating wake up config to {}", wakeUpWeek);
         return wakeUpService.updateWakeUpWeek(wakeUpWeek);
     }
 
     @PutMapping("/default")
     public WakeUpWeek defaultWakeUpConfig() {
+        log.info("Setting wake up config to default");
         return wakeUpService.setDefaultWakeUpWeek();
     }
 }

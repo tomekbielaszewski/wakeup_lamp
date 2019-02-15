69f56712c1856e20d327b0dc01f6a0b61c2dd693
tbielaszewski <tbielaszewski@objectivity.co.uk>
2019-02-15 12:42:37 +0100

    lampWorker test order refactored - alligns with actual lifecycle

--- src/test/java/pl/grizwold/wakeup_lamp/logic/LampWorkerTest.java
+++ src/test/java/pl/grizwold/wakeup_lamp/logic/LampWorkerTest.java
@@ -31,20 +31,20 @@ public class LampWorkerTest {
     private LampWorker lampWorker = new LampWorker();
 
     @Test
-    public void sets_light_power_to_max_when_close_to_end_time() {
+    public void lamp_is_shut_down_before_lightning_up() {
         setWakeUpDay(
                 LocalTime.of(1, 0),
                 LocalTime.of(10, 0)
         );
-        dimDelay(Duration.ofSeconds(10));
-        dimDuration(Duration.ofSeconds(10));
+        dimDelay(Duration.ofSeconds(1));
+        dimDuration(Duration.ofSeconds(1));
 
-        now(LocalTime.of(9, 59));
+        now(LocalTime.of(0, 59));
 
         lampWorker.scheduled();
 
         verify(raspberryPi).setPWM(argThat(pwm -> {
-                    assertThat(pwm, closeTo(MAX_PWM_RATE, 5));
+                    assertThat(pwm, closeTo(0, 5));
                     return true;
                 }
         ));
@@ -71,6 +71,26 @@ public class LampWorkerTest {
     }
 
     @Test
+    public void sets_light_power_to_max_when_close_to_end_time() {
+        setWakeUpDay(
+                LocalTime.of(1, 0),
+                LocalTime.of(10, 0)
+        );
+        dimDelay(Duration.ofSeconds(10));
+        dimDuration(Duration.ofSeconds(10));
+
+        now(LocalTime.of(9, 59));
+
+        lampWorker.scheduled();
+
+        verify(raspberryPi).setPWM(argThat(pwm -> {
+                    assertThat(pwm, closeTo(MAX_PWM_RATE, 5));
+                    return true;
+                }
+        ));
+    }
+
+    @Test
     public void keeps_max_value_during_dim_delay() {
         setWakeUpDay(
                 LocalTime.of(1, 0),
@@ -85,46 +105,6 @@ public class LampWorkerTest {
 
         verify(raspberryPi).setPWM(argThat(pwm -> {
                     assertThat(pwm, closeTo(MAX_PWM_RATE, 5));
-                    return true;
-                }
-        ));
-    }
-
-    @Test
-    public void shuts_down_the_lamp_after_dimming() {
-        setWakeUpDay(
-                LocalTime.of(1, 0),
-                LocalTime.of(10, 0)
-        );
-        dimDelay(Duration.ofSeconds(10));
-        dimDuration(Duration.ofSeconds(10));
-
-        now(LocalTime.of(10, 21));
-
-        lampWorker.scheduled();
-
-        verify(raspberryPi).setPWM(argThat(pwm -> {
-                    assertThat(pwm, closeTo(0, 5));
-                    return true;
-                }
-        ));
-    }
-
-    @Test
-    public void lamp_is_dimmed_at_the_end_of_dimming() {
-        setWakeUpDay(
-                LocalTime.of(1, 0),
-                LocalTime.of(2, 0)
-        );
-        dimDelay(Duration.ofSeconds(1));
-        dimDuration(Duration.ofMinutes(100000));
-
-        now(LocalTime.of(2, 0).plusMinutes(99999));
-
-        lampWorker.scheduled();
-
-        verify(raspberryPi).setPWM(argThat(pwm -> {
-                    assertThat(pwm, closeTo(0, 5));
                     return true;
                 }
         ));
@@ -151,15 +131,35 @@ public class LampWorkerTest {
     }
 
     @Test
-    public void lamp_is_shut_down_before_lightning_up() {
+    public void lamp_is_close_to_shut_down_at_the_end_of_dimming() {
+        setWakeUpDay(
+                LocalTime.of(1, 0),
+                LocalTime.of(2, 0)
+        );
+        dimDelay(Duration.ofSeconds(1));
+        dimDuration(Duration.ofMinutes(100000));
+
+        now(LocalTime.of(2, 0).plusMinutes(99999));
+
+        lampWorker.scheduled();
+
+        verify(raspberryPi).setPWM(argThat(pwm -> {
+                    assertThat(pwm, closeTo(0, 5));
+                    return true;
+                }
+        ));
+    }
+
+    @Test
+    public void shuts_down_the_lamp_after_dimming() {
         setWakeUpDay(
                 LocalTime.of(1, 0),
                 LocalTime.of(10, 0)
         );
-        dimDelay(Duration.ofSeconds(1));
-        dimDuration(Duration.ofSeconds(1));
+        dimDelay(Duration.ofSeconds(10));
+        dimDuration(Duration.ofSeconds(10));
 
-        now(LocalTime.of(0, 59));
+        now(LocalTime.of(10, 21));
 
         lampWorker.scheduled();
 
